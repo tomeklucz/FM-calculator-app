8b651f535aa4319fa556e825e63ccafadb4f9463
tomeklucz <tomasz.kluczynskii@gmail.com>
2022-08-20 17:32:38 +0200

    calculator app finished

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/* VARIABLES */
 const numberBtns = document.querySelectorAll(".btn-num");
 const operatorBtns = document.querySelectorAll(".btn-operator");
 const deleteBtn = document.querySelector(".btn-delete");
@@ -7,84 +8,106 @@ const resetBtn = document.querySelector(".btn-reset");
 const performBtn = document.querySelector(".btn-perform");
 const upperScreen = document.querySelector(".upper-screen");
 const mainScreen = document.querySelector(".main-screen");
-let currentNumber = "";
-let previousNumber;
-let currentOperator;
-let upperScreenText;
-let result;
 
-const roundLevel = 5;
-const numberMaxLength = 10;
+/* CALCULATOR APP */
+const calculatorApp = function () {
+  let currentNumber = "";
+  let previousNumber, currentOperator, upperScreenText, result;
 
-numberBtns.forEach((btn) =>
-  btn.addEventListener("click", function () {
-    if (result || result === 0) {
+  const roundLevel = 5;
+  const numberMaxLength = 10;
+
+  /* EVENT LISTENERS */
+  numberBtns.forEach((btn) =>
+    btn.addEventListener("click", function () {
+      if (result || result === 0) {
+        currentNumber = "";
+        result = "";
+      }
+      dotBtnCheck(btn);
+      if (currentNumber.length >= numberMaxLength) return;
+      currentNumber += btn.innerHTML;
+      mainScreenDisplay(currentNumber);
+    })
+  );
+
+  deleteBtn.addEventListener("click", function () {
+    if (result || result === 0) return;
+    currentNumber = currentNumber.slice(0, -1);
+    if (!currentNumber) currentNumber = "0";
+    mainScreenDisplay(currentNumber);
+  });
+
+  operatorBtns.forEach((btn) =>
+    btn.addEventListener("click", function () {
+      if (!currentNumber) currentNumber = "0";
+      currentOperator = btn.innerHTML;
+      if (result || result === 0) currentNumber = result;
+      previousNumber = currentNumber;
+      upperScreenText = previousNumber + " " + currentOperator;
+      upperScreenDisplay(upperScreenText);
       currentNumber = "";
-      result = "";
+    })
+  );
+
+  resetBtn.addEventListener("click", function () {
+    reset();
+  });
+
+  performBtn.addEventListener("click", function () {
+    calculate();
+  });
+
+  /* FUNCTIONS */
+  const calculate = function () {
+    const roundValue = 10 ** roundLevel;
+    console.log(previousNumber, currentNumber, currentOperator, result);
+    if (!previousNumber || !currentNumber || !currentOperator) return;
+    previousNumber = Number(previousNumber);
+    currentNumber = Number(currentNumber);
+    switch (currentOperator) {
+      case "+":
+        result = previousNumber + currentNumber;
+        break;
+      case "-":
+        result = previousNumber - currentNumber;
+        break;
+      case "x":
+        result = previousNumber * currentNumber;
+        break;
+      case "/":
+        result = previousNumber / currentNumber;
+        break;
     }
+    result = Math.round(result * roundValue) / roundValue;
+    mainScreenDisplay(result);
+    upperScreenDisplay("");
+    previousNumber = result;
+  };
+
+  const reset = function () {
+    currentNumber = "";
+    previousNumber = "";
+    result = "";
+    upperScreenText = "";
+    upperScreenDisplay("");
+    mainScreenDisplay("0");
+    currentOperator = "";
+  };
+
+  const dotBtnCheck = function (btn) {
     if (btn.innerHTML === ".") {
       if (currentNumber.includes(".")) return;
       if (!currentNumber) currentNumber = "0";
     }
-    if (currentNumber.length >= numberMaxLength) return;
-    currentNumber += btn.innerHTML;
-    mainScreen.innerHTML = currentNumber;
-  })
-);
+  };
 
-deleteBtn.addEventListener("click", function () {
-  currentNumber = currentNumber.slice(0, -1);
-  mainScreen.innerHTML = currentNumber;
-});
+  const mainScreenDisplay = function (textToDisplay) {
+    mainScreen.innerHTML = textToDisplay;
+  };
 
-operatorBtns.forEach((btn) =>
-  btn.addEventListener("click", function () {
-    if (!currentNumber) currentNumber = "0";
-    currentOperator = btn.innerHTML;
-    if (result || result === 0) currentNumber = result;
-    previousNumber = currentNumber;
-    upperScreenText = previousNumber + " " + currentOperator;
-    upperScreen.innerHTML = upperScreenText;
-    currentNumber = "";
-  })
-);
-
-resetBtn.addEventListener("click", function () {
-  currentNumber = "";
-  previousNumber = "";
-  result = "";
-  upperScreenText = "";
-  upperScreen.innerHTML = "";
-  mainScreen.innerHTML = "0";
-  currentOperator = "";
-});
-
-performBtn.addEventListener("click", function () {
-  calculate();
-});
-
-const calculate = function () {
-  console.log(previousNumber, currentNumber, currentOperator, result);
-  const roundValue = 10 ** roundLevel;
-  const prev = Number(previousNumber);
-  const curr = Number(currentNumber);
-  if (!previousNumber || !currentNumber || !currentOperator) return;
-  switch (currentOperator) {
-    case "+":
-      result = prev + curr;
-      break;
-    case "-":
-      result = prev - curr;
-      break;
-    case "x":
-      result = prev * curr;
-      break;
-    case "/":
-      result = prev / curr;
-      break;
-  }
-  result = Math.round(result * roundValue) / roundValue;
-  mainScreen.innerHTML = result;
-  upperScreen.innerHTML = "";
-  previousNumber = result;
+  const upperScreenDisplay = function (textToDisplay) {
+    upperScreen.innerHTML = textToDisplay;
+  };
 };
+calculatorApp();
